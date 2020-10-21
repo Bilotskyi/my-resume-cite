ff495e8e986a0ab949853ec2321fb6ee1b9a964c
Bilotskyi <73232224+Bilotskyi@users.noreply.github.com>
2020-10-22 00:04:53 +0300

    Add all project files

---
+++ js/project-modals.js
@@ -0,0 +1,51 @@
+const projectModalFullStack = document.querySelector("#personal-project-full-stack");
+const projectModalWeddingProject = document.querySelector("#personal-project-wedding-project");
+const projectModaProTest = document.querySelector("#personal-project-pro-test");
+const projectModalProjectGoit = document.querySelector("#personal-project-goit");
+
+const projectModalQuestify = document.querySelector("#team-project-questify");
+const projectModalItTest = document.querySelector("#team-project-it-test");
+
+// const modalThanks = document.querySelector("#thanks");
+
+const projectOpenBtnFullStack = document.querySelector("#personal-project-full-stack-btn");
+const projectOpenBtnWeddingProject = document.querySelector("#personal-project-wedding-project-btn");
+const projectOpenBtnProTest = document.querySelector("#personal-project-pro-test-btn");
+const projectOpenBtnProjectGoit = document.querySelector("#personal-project-goit-btn");
+
+const projectOpenBtnQuestify = document.querySelector("#team-project-questify-btn");
+const projectOpenBtnItTest = document.querySelector("#team-project-it-test-btn");
+
+// const callbackSubmit = document.querySelector(".callback-form-submit");
+
+const projectModals = [
+   projectModalFullStack,
+   projectModalWeddingProject,
+   projectModaProTest,
+   projectModalProjectGoit,
+   projectModalQuestify,
+   projectModalItTest,
+   // modalThanks,
+];
+const projectBtns = [
+   projectOpenBtnFullStack,
+   projectOpenBtnWeddingProject,
+   projectOpenBtnProTest,
+   projectOpenBtnProjectGoit,
+   projectOpenBtnQuestify,
+   projectOpenBtnItTest,
+   // callbackSubmit,
+];
+
+projectBtns.forEach((btn, index) => {
+   const projectModal = projectModals[index];
+
+   if (btn) {
+      btn.addEventListener("click", (event) => {
+         event.preventDefault();
+         projectModal.classList.add(MODAL_ACTIVE_CLASS);
+
+         document.body.classList.add(BODY_SCROLL_DISABLE_CLASS);
+      });
+   }
+});
